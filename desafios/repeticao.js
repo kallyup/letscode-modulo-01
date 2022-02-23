fb2fd57d3db784543deb61cebf914512aaaafc60
perkles <deboramacedo8816@gmail.com>
2022-02-23 11:21:24 -0300

    lógicas

---
+++ desafios/repeticao.js
@@ -0,0 +1,153 @@
+console.log("---repetição---")
+
+/*let inicial = 50;
+let final = 1;
+while(inicial >= final){
+    document.write(inicial);
+    inicial--;
+}
+document.write("<hr>")
+
+let ini = 0;
+let fin= 100;
+while(ini <= fin){
+    document.write(ini, " ")
+    ini+=2;
+}
+document.write("<hr>")
+let i = 99;
+let f = 1 
+while( i >= f ){
+    if (i ==99){
+        document.write(i);
+    }else{
+        document.write("- ", i);
+    }
+    i-=2
+}
+var inicial = 99;
+var final = 0;
+
+while (inicial >= final) {
+    document.write(`${inicial}`);
+    if (inicial > 1) {
+        document.write("-")
+    }  
+    inicial = inicial - 2;
+}
+document.write("<hr>");
+let numex4 = 99;
+
+while (numex4 >= 1) {
+    if (numex4 != 1) {
+        document.write(numex4, " - ");
+    } else {
+        document.write(numex4);
+    }
+    numex4 -= 2;
+}
+var valor = 99;
+
+while (valor != 0){
+    if (valor % 2 != 0){
+        document.write(valor);
+        if (valor != 1){
+            document.write( " - ");
+        }
+        
+    }
+    valor = valor - 1;
+}
+document.write("<br><br>Números ímpares de 99 a 1:<br>")
+inicial = 99;
+final = 1;
+while (inicial >= final) {
+    if (inicial == 1) {
+        document.write(inicial);
+        break;
+    }
+    else {
+        document.write(inicial, " - ");
+        inicial -= 2;
+    }
+}
+document.write("<br><br>Números ímpares de 99 a 1:<br>")
+inicial = 99;
+final = 1;
+while (inicial >= final) {
+    if (inicial == 1) {
+        document.write(inicial);
+        break;
+    }
+    else {
+        document.write(inicial, " - ");
+        inicial -= 2;
+    }
+}
+while (inicial4 >= final4) {
+    if (inicial4 == 1) {
+        document.write(inicial4);
+        break;
+    }
+    document.write(inicial4, " - ");
+    inicial4 -= 2;
+}
+document.write("<hr>")
+ixtarti = 99;
+finau = 1;
+
+while (ixtarti >= finau) {
+    if(ixtarti % 2 == 1){
+        if (ixtarti == finau){
+            document.write(ixtarti);
+        } else {
+            document.write(ixtarti," - ");
+        }
+}
+    ixtarti--;
+}
+i = 99
+
+while (i >= 1) {
+
+    switch (i) {
+        case 1:
+            document.write(i)
+            break;
+    
+        default:
+            document.write(i, ' - ')
+            break;
+    }
+    i -= 2
+}
+var x = prompt("digite um numero");
+x =parseInt(x)
+while(x!=0){
+    console.log(x)
+    document.write(x)
+    x= prompt("informe outro numero")
+    }
+console.log('fim')
+
+ let loop = true;
+ let num;
+ while(loop){
+     num=prompt9("digite um numero");
+     console.log(num);
+     if (num==0){
+         console.log("fim")
+         loop=false;
+     }
+ }*/
+ let ast= parseInt(prompt("informe o valor"));
+  for(let i=0; i<=ast; i++){
+      document.write("*");
+      console.log("*  ")
+  }
+for(let lin=0;lib<5;lin++){
+    for(let col=0; col<10; col++){
+        document.write("*")
+    }
+    document.write("<br>")
+}
---
+++ lógica de programação exemplos/exercicios.js
@@ -0,0 +1,45 @@
+//IMPRIMA HELLO PETER
+let input= "peter";
+document.write("hello ", input, "!");
+//SOMA DE DOIS NUMEROS
+let n1 = 7;
+let n2 = 8;
+document.write(n1+n2);
+console.log(n1+n2)
+//IMPRIMA O MAIOR NUMERO
+let a = 22;
+let b =43;
+let c= 32
+
+if(a > b && a>c){
+    document.write(a);
+}else if(b>a && b>c){
+    document.write(b);
+}else{
+    document.write(c);
+}
+//MULTIPLIQUE 2 NUMEROS
+let n1 = 7;
+let n2 = 8;
+document.write(n1*n2);
+console.log(n1*n2)
+//FAÇA ELEVE AO QUADRADO
+//let n1 = 7;
+let n2 = 8;
+document.write(n2*n2);
+console.log(n2*n2)
+//FAÇA MEDIA
+let n1 = 7;
+let n2 = 8;
+let n3 = 4;
+let resul = n1 + n2 + n2;
+document.write(resul/3);
+//console.log(n1*n2)
+// FATORIAL
+let soma=5;
+for (let i=4; i>1;i--){
+  soma=soma*(i);
+}
+  console.log(soma);
+
+
